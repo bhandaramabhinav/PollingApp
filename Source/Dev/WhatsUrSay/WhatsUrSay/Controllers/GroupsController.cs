c562aea33fc4debcb8b86d2db762d6b5b7969242
Goud's <Goud's@DESKTOP-9UJNJSD>
2016-11-14 22:48:37 -0600

    Added comments

--- Source/Dev/WhatsUrSay/WhatsUrSay/Controllers/GroupsController.cs
+++ Source/Dev/WhatsUrSay/WhatsUrSay/Controllers/GroupsController.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/* File Name :GroupsController.cs
+ * Created By: Raj 
+ * This File resides in Business Layer 
+ * 
+ * Change History
+ ************************
+ ** PR   Date        Author         Description 
+ ** --   --------   -------   ------------------------------------
+ ** 1    11/7/2016     Raj      Created GroupsController class and incuded methods  to create ,delete,edit group details
+ * This file hold business logic related to group related activities and provides services to UI layer up on request.
+ * 
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -18,12 +32,22 @@ namespace WhatsUrSay.Controllers
         GroupsRepository groupRepository = new GroupsRepository();
 
         // GET: api/Groups
+        /// <summary>
+        /// This method returns all groups listed in database
+        /// </summary>
+        /// <returns></returns>
         public IQueryable<Group> GetGroups()
         {
+           
             return groupRepository.GetGroups();
         }
 
         // GET: api/Groups/5
+        /// <summary>
+        /// This method get specific group details 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>group details</returns>
         [ResponseType(typeof(GroupDetails))]
         public IHttpActionResult GetGroup([FromUri]int id)
         {
@@ -32,7 +56,12 @@ namespace WhatsUrSay.Controllers
             return Ok(groupDetails);
         }
 
-        // PUT: api/Groups/5
+        // PUT: api/Groups/5/
+        /// <summary>
+        /// This method updates specific group details
+        /// </summary>
+        /// <param name="group"></param>
+        /// <returns> status code</returns>
         [ResponseType(typeof(void))]
         public IHttpActionResult PutGroup(Group group)
         {
@@ -55,6 +84,11 @@ namespace WhatsUrSay.Controllers
         }
 
         // POST: api/Groups
+        /// <summary>
+        /// This method  creates new group
+        /// </summary>
+        /// <param name="group"></param>
+        /// <returns>Route of created group</returns>
         [ResponseType(typeof(Group))]
         public IHttpActionResult PostGroup(Group group)
         {
@@ -78,10 +112,25 @@ namespace WhatsUrSay.Controllers
         }
 
         // DELETE: api/Groups/5
+        /// <summary>
+        /// This method deletes specific group
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> deleted group details</returns>
         [ResponseType(typeof(Group))]
         public IHttpActionResult DeleteGroup([FromUri]int id)
         {
-            Group group = groupRepository.DeleteGroup(id);
+            Group group;
+            try
+            {
+                 group = groupRepository.DeleteGroup(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
 
             return Ok(group);
         }
