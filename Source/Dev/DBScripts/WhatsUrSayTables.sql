8864c77f896c52a9aba13d30306b4f02e7079b5f
sreedevi-koppula <sreedevi.519.07@gmail.com>
2016-11-18 22:57:12 -0600

    Added new tables to the database and modified the existing tables

--- Source/Dev/DBScripts/WhatsUrSayTables.sql
+++ Source/Dev/DBScripts/WhatsUrSayTables.sql
@@ -14,44 +14,101 @@ Component usage of data structures, algorithms and control(if any):
 CREATE DATABASE [DSE]
 GO 
 
-/*Uses the database 'DSE' to perform the below SQL operations*/
 USE [DSE]
 GO
 
-/* Object:  Table [dbo].[User] */
+/****** Object:  Table [dbo].[Activity]    Script Date: 11/18/2016 10:43:03 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[User](
-	[id] [int] NOT NULL,
-	[name] [ntext] NOT NULL,
-	[role] [ntext] NULL,
-	[emailId] [ntext] NOT NULL,
-	[pwd] [ntext] NOT NULL,
-	[status] [ntext] NOT NULL,
- CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Category](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[Type](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Type] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
 
 
-/* Object:  Table [dbo].[Group] */
+CREATE TABLE [dbo].[User](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](100) NOT NULL,
+	[role] [nvarchar](50) NOT NULL,
+	[emailId] [nvarchar](50) NOT NULL,
+	[pwd] [nvarchar](50) NOT NULL,
+	[status] [nvarchar](50) NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
+
+CREATE TABLE [dbo].[Activity](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[heading] [nvarchar](300) NOT NULL,
+	[description] [nvarchar](1000) NULL,
+	[type] [int] NOT NULL,
+	[category] [int] NOT NULL,
+	[createdby] [int] NOT NULL,
+ CONSTRAINT [PK_Activity] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Activity]  WITH CHECK ADD  CONSTRAINT [FK_Activity_Category] FOREIGN KEY([category])
+REFERENCES [dbo].[Category] ([id])
+GO
+
+ALTER TABLE [dbo].[Activity] CHECK CONSTRAINT [FK_Activity_Category]
+GO
+
+ALTER TABLE [dbo].[Activity]  WITH CHECK ADD  CONSTRAINT [FK_Activity_Type] FOREIGN KEY([type])
+REFERENCES [dbo].[Type] ([id])
+GO
+
+ALTER TABLE [dbo].[Activity] CHECK CONSTRAINT [FK_Activity_Type]
+GO
+
+ALTER TABLE [dbo].[Activity]  WITH CHECK ADD  CONSTRAINT [FK_Activity_User] FOREIGN KEY([createdby])
+REFERENCES [dbo].[User] ([id])
+GO
+
+ALTER TABLE [dbo].[Activity] CHECK CONSTRAINT [FK_Activity_User]
+GO
+
 CREATE TABLE [dbo].[Group](
-	[id] [int] NOT NULL,
-	[name] [ntext] NOT NULL,
-	[user_ids] [ntext] NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](300) NOT NULL,
 	[createdby] [int] NOT NULL,
  CONSTRAINT [PK_Group] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
 
@@ -63,87 +120,41 @@ ALTER TABLE [dbo].[Group] CHECK CONSTRAINT [FK_Group_User]
 GO
 
 
-/* Object:  Table [dbo].[User_Group] */
-CREATE TABLE [dbo].[User_Group](
-	[id] [int] NOT NULL,
-	[user_id] [int] NOT NULL,
-	[group_ids] [ntext] NULL,
- CONSTRAINT [PK_User_Group] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Activity_Group](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[activity_id] [int] NOT NULL,
+	[group_id] [int] NOT NULL,
+ CONSTRAINT [PK_Activity_Group] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
 
-ALTER TABLE [dbo].[User_Group]  WITH CHECK ADD  CONSTRAINT [FK_User_Group_User] FOREIGN KEY([user_id])
-REFERENCES [dbo].[User] ([id])
+ALTER TABLE [dbo].[Activity_Group]  WITH CHECK ADD  CONSTRAINT [FK_Activity_Group_Activity] FOREIGN KEY([activity_id])
+REFERENCES [dbo].[Activity] ([id])
 GO
 
-ALTER TABLE [dbo].[User_Group] CHECK CONSTRAINT [FK_User_Group_User]
+ALTER TABLE [dbo].[Activity_Group] CHECK CONSTRAINT [FK_Activity_Group_Activity]
 GO
 
-
-
-/* Object:  Table [dbo].[User_Request] */
-CREATE TABLE [dbo].[User_Request](
-	[id] [int] NOT NULL,
-	[user_id] [int] NOT NULL,
-	[description] [ntext] NULL,
-	[status] [int] NOT NULL,
- CONSTRAINT [PK_User_Request] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
+ALTER TABLE [dbo].[Activity_Group]  WITH CHECK ADD  CONSTRAINT [FK_Activity_Group_Group] FOREIGN KEY([group_id])
+REFERENCES [dbo].[Group] ([id])
 GO
 
-ALTER TABLE [dbo].[User_Request]  WITH CHECK ADD  CONSTRAINT [FK_User_Request_User] FOREIGN KEY([user_id])
-REFERENCES [dbo].[User] ([id])
+ALTER TABLE [dbo].[Activity_Group] CHECK CONSTRAINT [FK_Activity_Group_Group]
 GO
 
-ALTER TABLE [dbo].[User_Request] CHECK CONSTRAINT [FK_User_Request_User]
-GO
-
-
-
-
-/* Object:  Table [dbo].[Activity] */
-CREATE TABLE [dbo].[Activity](
-	[id] [int] NOT NULL,
-	[heading] [ntext] NOT NULL,
-	[description] [ntext] NULL,
-	[type] [ntext] NOT NULL,
-	[category] [ntext] NOT NULL,
-	[group_ids] [ntext] NULL,
-	[createdby] [int] NOT NULL,
- CONSTRAINT [PK_Activity] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-GO
-
-ALTER TABLE [dbo].[Activity]  WITH CHECK ADD  CONSTRAINT [FK_Activity_User] FOREIGN KEY([createdby])
-REFERENCES [dbo].[User] ([id])
-GO
-
-ALTER TABLE [dbo].[Activity] CHECK CONSTRAINT [FK_Activity_User]
-GO
-
-
-/* Object:  Table [dbo].[Question] */
 CREATE TABLE [dbo].[Question](
-	[id] [int] NOT NULL,
-	[description] [ntext] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[description] [nvarchar](1000) NULL,
 	[activity_id] [int] NOT NULL,
  CONSTRAINT [PK_Question] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
 
@@ -154,10 +165,10 @@ GO
 ALTER TABLE [dbo].[Question] CHECK CONSTRAINT [FK_Question_Activity]
 GO
 
-/* Object:  Table [dbo].[Answer] */
+
 CREATE TABLE [dbo].[Answer](
-	[id] [int] NOT NULL,
-	[description] [ntext] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[description] [nvarchar](500) NULL,
 	[question_id] [int] NOT NULL,
 	[activity_id] [int] NOT NULL,
 	[count] [int] NULL,
@@ -165,7 +176,7 @@ CREATE TABLE [dbo].[Answer](
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
 
@@ -182,3 +193,96 @@ GO
 
 ALTER TABLE [dbo].[Answer] CHECK CONSTRAINT [FK_Answer_Question]
 GO
+
+
+CREATE TABLE [dbo].[User_Answer](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[activity_id] [int] NOT NULL,
+	[question_id] [int] NOT NULL,
+	[answer_id] [int] NOT NULL,
+ CONSTRAINT [PK_User_Answer] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[User_Answer]  WITH CHECK ADD  CONSTRAINT [FK_User_Answer_Answer] FOREIGN KEY([answer_id])
+REFERENCES [dbo].[Answer] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Answer] CHECK CONSTRAINT [FK_User_Answer_Answer]
+GO
+
+ALTER TABLE [dbo].[User_Answer]  WITH CHECK ADD  CONSTRAINT [FK_User_Answer_User] FOREIGN KEY([user_id])
+REFERENCES [dbo].[User] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Answer] CHECK CONSTRAINT [FK_User_Answer_User]
+GO
+
+ALTER TABLE [dbo].[User_Answer]  WITH CHECK ADD  CONSTRAINT [FK_User_Answer_User_Activity] FOREIGN KEY([activity_id])
+REFERENCES [dbo].[Activity] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Answer] CHECK CONSTRAINT [FK_User_Answer_User_Activity]
+GO
+
+ALTER TABLE [dbo].[User_Answer]  WITH CHECK ADD  CONSTRAINT [FK_User_Answer_User_Question] FOREIGN KEY([question_id])
+REFERENCES [dbo].[Question] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Answer] CHECK CONSTRAINT [FK_User_Answer_User_Question]
+GO
+
+
+CREATE TABLE [dbo].[User_Group](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[group_id] [int] NOT NULL,
+ CONSTRAINT [PK_User_Group] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[User_Group]  WITH CHECK ADD  CONSTRAINT [FK_User_Group_Group] FOREIGN KEY([group_id])
+REFERENCES [dbo].[Group] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Group] CHECK CONSTRAINT [FK_User_Group_Group]
+GO
+
+ALTER TABLE [dbo].[User_Group]  WITH CHECK ADD  CONSTRAINT [FK_User_Group_User] FOREIGN KEY([user_id])
+REFERENCES [dbo].[User] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Group] CHECK CONSTRAINT [FK_User_Group_User]
+GO
+
+
+CREATE TABLE [dbo].[User_Request](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[description] [nvarchar](1000) NULL,
+	[status] [int] NOT NULL,
+ CONSTRAINT [PK_User_Request] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[User_Request]  WITH CHECK ADD  CONSTRAINT [FK_User_Request_User] FOREIGN KEY([user_id])
+REFERENCES [dbo].[User] ([id])
+GO
+
+ALTER TABLE [dbo].[User_Request] CHECK CONSTRAINT [FK_User_Request_User]
+GO
+
+
